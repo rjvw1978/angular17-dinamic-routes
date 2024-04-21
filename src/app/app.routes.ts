a787ee15ea17637193baef97c226868abbb9c37d
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-04-21 18:11:10 -0300

    update to enhance the folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { RegistroComponent } from './pages/auth/registro/registro.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProfesionalComponent } from './pages/profesional/profesional.component';
+import { ProfesionalComponent } from './pages/quienes-somos/profesional/profesional.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path:"home", component:DashboardComponent},
+    {path:"home", component:HomeComponent},
+    {path:"panel-control", component:DashboardComponent},
     {path:"iniciar-sesion", component:IniciarSesionComponent},
     {path:"registro", component:RegistroComponent},
     {path:"quienes-somos", component:QuienesSomosComponent},
